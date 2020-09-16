63fa58d3cf8338df83ad62b514b7841bf4a1362d
Miguel Ángel Latre Abadía <latre@unizar.es>
2020-09-16 19:10:25 +0200

    Ajustado a 80 caracteres de ancho

--- 3-eco-numerico.cpp
+++ 3-eco-numerico.cpp
@@ -2,7 +2,8 @@
  * Curso de Programación 1. Tema 1
  * Autores: Miguel Ángel Latre
  * Última revisión: 16 de septiembre de 2020
- * Resumen: Programa que solicita un número entero al usuario y lo escribe en pantalla.
+ * Resumen: Programa que solicita un número entero al usuario y lo escribe en 
+ *          pantalla.
  */
 #include <iostream>
 using namespace std;
