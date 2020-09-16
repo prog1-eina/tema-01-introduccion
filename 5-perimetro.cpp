1285b4a0021f48bf5d6344e1ec6363d3dd6d2e2b
Miguel Ángel Latre Abadía <latre@unizar.es>
2020-09-16 19:09:24 +0200

    Ajustado 5-perimetro.cpp a 80 caracteres de ancho

--- 5-perimetro.cpp
+++ 5-perimetro.cpp
@@ -2,24 +2,27 @@
  * Curso de Programación 1. Tema 1
  * Autores: Miguel Ángel Latre
  * Última revisión: 16 de septiembre de 2020
- * Resumen: Programa que solicita al usuario el número de lados de un polígono regular y la longitud de sus lados
- *          y escribe en la pantalla el área de dicho polígono regular.
+ * Resumen: Programa que solicita al usuario el número de lados de un polígono
+ *          regular y la longitud de sus lados y escribe en la pantalla el área 
+ *          de dicho polígono regular.
  */
 #include <iostream>
 using namespace std;
 
 /*
- * Dado un polígono regular con un número de lados igual al valor del parámetro «numLados»
- * de longitud igual al valor del parámetro «longitud», devuelve el perímetro de dicho polígono
- * regular. «numLados» tiene que ser mayor o igual que 3 y «longitud» mayor que 0.0.
+ * Dado un polígono regular con un número de lados igual al valor del parámetro 
+ * «numLados» de longitud igual al valor del parámetro «longitud», devuelve el
+ * perímetro de dicho polígono regular. «numLados» tiene que ser mayor o igual 
+ * que 3 y «longitud» mayor que 0.0.
  */
 double perimetro(int numLados, double longitud) {
     return numLados * longitud;
 }
 
 /*
- * Programa que solicita al usuario el número de lados de un polígono regular y la longitud de 
- * sus lados y escribe en la pantalla el área de dicho polígono regular.
+ * Programa que solicita al usuario el número de lados de un polígono regular y 
+ * la longitud de sus lados y escribe en la pantalla el área de dicho polígono
+ * regular.
  */
 int main() {
     cout << "Escriba el número de lados: ";
@@ -31,8 +34,8 @@ int main() {
     cin >> longitud;
 
     cout << "El perímetro de un polígono regular de " << numLados
-         << " lados de longitud " << longitud << " es " << perimetro(numLados, longitud)
-         << "." << endl;
+         << " lados de longitud " << longitud << " es " 
+         << perimetro(numLados, longitud) << "." << endl;
 
     return 0;
 }
