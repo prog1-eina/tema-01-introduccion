6530d89025656fcd9b935e591cd1aae6e872a469
Miguel Ángel Latre Abadía <latre@unizar.es>
2020-09-16 19:11:01 +0200

    Ajustado a 80 caracteres de ancho

--- 4-circulo.cpp
+++ 4-circulo.cpp
@@ -2,8 +2,8 @@
  * Curso de Programación 1. Tema 1
  * Autores: Miguel Ángel Latre
  * Última revisión: 17 de septiembre de 2019
- * Resumen: Programa que solicita al usuario la longitud de un radio y escribe en la
- *          pantalla el área del círculo correspondiente.
+ * Resumen: Programa que solicita al usuario la longitud de un radio y escribe 
+ *          en la pantalla el área del círculo correspondiente.
  */
 #include <iostream>
 using namespace std;
@@ -20,7 +20,8 @@ int main() {
     double r;
     cin >> r;
     
-    cout << "El área de un círculo de radio " << r << " es " << PI * r * r << endl;
+    cout << "El área de un círculo de radio " << r << " es " 
+         << PI * r * r << endl;
     
     return 0;
 }
